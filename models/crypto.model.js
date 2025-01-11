6c0c0af79d0f0b5db32adcf7b3725d9e9a3364b9
adiityyya <cocaslps@gmail.com>
2025-01-11 06:41:18 +0530

    created schema and added indexing for optimized queries

---
+++ models/crypto.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const cryptoSchema = new mongoose.Schema({
+    coin: { type: String, required: true },
+    price: { type: Number, required: true },
+    marketCap: { type: Number, required: true },
+    change24h: { type: Number, required: true },
+    timestamp: { type: Date, default: Date.now },
+});
+
+cryptoSchema.index({ coin: 1, timestamp: -1 });
+
+const Crypto = mongoose.model("Crypto", cryptoSchema);
+
+export default Crypto;
